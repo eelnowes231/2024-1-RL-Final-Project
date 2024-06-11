bca37abf99c86c3c287103590cbb2fbf53aaf143
argus <tjdals4047@gmail.com>
2024-06-11 15:06:05 +0900

    modify train.py dataload

--- train.py
+++ train.py
@@ -1,51 +1,35 @@
 # Dependencies
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import itertools
-import matplotlib.pyplot as plt
-import time
+import os
 
-from data import load_interact_dataset, load_whole_dataset
+from data import load_dataset
 from envs import OfflineEnv
 from recommender import DRRAgent
+"""
+[Training 방식]
+- 매 에피소드마다 user를 랜덤하게 선택 (OfflineEnv.user)
+- user의 최근에 본 영화 10개를 이용해 state 생성
+- user가 최근에 본 영화 10개를 제외한 나머지 영화들을 추천 (DRRAgent.recommend_item())
+- 한 user당 trajectory 최대 길이는 약 3000, 그 전에 user가 본 영화 history 길이만큼 추천받으면 종료(OfflineEnv.step()) 
+- Actor, Critic 파라미터 업데이트는 replay buffer에서 32개씩 batch로 묶어서 진행
+"""
 
-import os
 ROOT_DIR = os.getcwd()
 DATA_DIR = os.path.join(ROOT_DIR, 'data/ml-1m/')
-STATE_SIZE = 5
+STATE_SIZE = 10
+TOP_K = 5
 MAX_EPISODE_NUM = 8000
 
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
 if __name__ == "__main__":
 
-    # Loading datasets - whole dataset 
-    # ratings_df, users_dict, users_history_lens, movies_id_to_movies = load_whole_dataset(DATA_DIR)
+    # Loading dataset
+    users_num, items_num, train_users_dict, users_history_lens, movies_id_to_movies = load_dataset(DATA_DIR, 'train')
 
-    # Loading dataset v2 - interacted items only  
-    ratings_df, users_dict, users_history_lens, movies_id_to_movies = load_interact_dataset(DATA_DIR)
-
-    # 6039명의 user, 2819개의 영화
-    users_num = max(ratings_df["UserID"])+1
-    items_num = max(ratings_df["MovieID"])+1
-    
-    # Training setting
-    train_users_num = int(users_num * 0.8)
-    train_items_num = items_num
-    train_users_dict = {k: users_dict.item().get(k) for k in range(train_users_num)}
-    train_users_history_lens = users_history_lens[:train_users_num]
-
-    print('DONE!')
-    time.sleep(2)
-
-    TOP_K = 5
-    env = OfflineEnv(train_users_dict, train_users_history_lens,
-                     movies_id_to_movies, STATE_SIZE)
+    env = OfflineEnv(train_users_dict, users_history_lens, movies_id_to_movies, STATE_SIZE)
     print(f"Available number of users: {len(env.available_users)}")
   
-    recommender = DRRAgent(env, users_num, items_num,
-                           STATE_SIZE, use_wandb=False)
+    recommender = DRRAgent(env, users_num, items_num, STATE_SIZE, use_wandb=False)
     recommender.actor.build_networks()
     recommender.critic.build_networks()
     recommender.train(MAX_EPISODE_NUM, load_model=False, top_k=TOP_K)
